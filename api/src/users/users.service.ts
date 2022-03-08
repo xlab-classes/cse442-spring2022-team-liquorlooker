49f9c618b4b4c9f314783b86483daa0c47ecc0ef
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-07 22:33:13 -0500

    Add user repository to user service for db operations #38

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+}
