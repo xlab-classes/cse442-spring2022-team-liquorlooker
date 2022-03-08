82ae8d507a1555ad40c8f34cc5d526cebb88aff2
Naqi Haider <naqihaid@buffalo.edu>
2022-03-08 09:43:58 -0500

    Updated the navbar and added css #34

--- frontend/src/components/Navbar/Layout.js
+++ frontend/src/components/Navbar/Layout.js
@@ -1,29 +1,28 @@
 import * as React from "react";
 import { Outlet, Link } from "react-router-dom";
+import "./Layout.css"
 
 const Layout = () => {
     return (
-      <div>
+      <div className="container">
         {/* A "layout route" is a good place to put markup you want to
-            share across all the pages on your site, like navigation. */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">LiquorLooker</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-  
-        <hr />
+            share across all the pages on your site, like navigation. */} 
+          
+          <nav className="navbar">
+            <Link to="/" className="logo">LiquorLooker</Link>
+            <ul className="nav-links">
+              <li className="nav-item">
+                <Link to="/login" className="link">Login</Link>
+              </li>
+            </ul>
+          </nav>
   
         {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
             the child routes we defined above. */}
         <Outlet />
       </div>
+      
     );
   }
 
