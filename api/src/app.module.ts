eea5c3861de44984517cbddc0f8ca7c207773809
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-07 22:31:59 -0500

    Database connection defined in app module #38

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'oceanus.cse.buffalo.edu',
+      port: 3306,
+      username: 'mcfocacc',
+      password: '50235965',
+      database: 'mcfocacc_db',
+      entities: [User],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
