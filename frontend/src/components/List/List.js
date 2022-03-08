ec276a2d46b839f2360b275b78182bb34c5a64e5
Naqi Haider <naqihaid@buffalo.edu>
2022-03-08 09:45:48 -0500

    Added a scrollable list view #34

---
+++ frontend/src/components/List/List.js
@@ -0,0 +1,38 @@
+import React from "react";
+import data from "./store.json";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+const MyList = (props) => {
+  return (
+    // <ul>
+    //     {data.map((item) => (
+    //         <li key={item.id}>{item.text}</li>
+    //     ))}
+    // </ul>
+    <List
+      sx={{
+        width: "100%",
+        maxWidth: 360,
+        bgcolor: "background.paper",
+        position: "relative",
+        overflow: "auto",
+        maxHeight: '90vh',
+        overflow: 'auto',
+        "& ul": { padding: 0 },
+      }}
+    >
+      
+    <ul>
+        {data.map((item) => (
+            <ListItem key={`${item.id}`}>
+                <ListItemText primary={`${item.text}`} />
+            </ListItem>
+        ))}
+    </ul>
+    </List>
+  );
+};
+
+export default MyList;
