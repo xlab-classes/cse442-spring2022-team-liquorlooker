b350592ae27044baac744d0eb46f3e08269e2bd7
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-08 20:08:16 -0500

    Create user in database if email isn't taken , return user #45

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService) {}
+
+    async register(email: string, password: string): Promise<User> {
+        return await this.usersService.createUser(email, password);
+    }
+}
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -10,10 +10,26 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  findByEmail(email: string): Promise<User> {
-    return this.usersRepository
+  async userWithEmailExists(email: string): Promise<boolean> {
+    const user = await this.usersRepository
       .createQueryBuilder('user')
       .where('user.email = :email', { email: email })
       .getOne();
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+
+  async createUser(email: string, password: string): Promise<User> {
+    if (await this.userWithEmailExists(email)) {
+      return null;
+    }
+    const user = await this.usersRepository.create({
+      email: email,
+      password: password
+    });
+    await this.usersRepository.save(user);
+    return user;
   }
 }
