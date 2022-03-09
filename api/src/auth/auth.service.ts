abee7e8c4447f939f1063a2d4f2a3e002444f414
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-08 20:24:02 -0500

    If user with email exists and password matches, returns user #46

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/users.entity';
 
@@ -8,5 +8,13 @@ export class AuthService {
 
     async register(email: string, password: string): Promise<User> {
         return await this.usersService.createUser(email, password);
+    }
+
+    async login(email: string, password: string): Promise<User> {
+        const user = await this.usersService.getUserByEmail(email);
+        if (user && user.password === password) {
+            return user;
+        }
+        throw HttpStatus.UNAUTHORIZED;
     }
 }
