8d53c3c15bc220bd1506c454eaf6694a210de23a
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-08 20:07:31 -0500

    Declared 'auth/register' route #45

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthDto } from './auth/auth.dto';
+import { AuthService } from './auth/auth.service';
+import { User } from './users/users.entity';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('auth/register')
+  async register(@Body() credentials: AuthDto): Promise<User> {
+    const {email, password} = credentials;
+    return await this.authService.register(email, password);
   }
 }
