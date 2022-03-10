137113227f97d443157c8b541e3e9fdcb15a56b3
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-10 15:45:48 -0500

    Add @Exclude tag to password field in user entity #50

--- api/src/users/users.entity.ts
+++ api/src/users/users.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -8,6 +9,7 @@ export class User {
   @Column({ nullable: false, unique: true })
   email: string;
 
+  @Exclude()
   @Column({ nullable: false })
   password: string;
 }
