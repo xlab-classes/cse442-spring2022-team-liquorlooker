85cf57abf187bafac64c1adf61f744edbc87b4a2
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-09 22:03:16 -0500

    Add login function which returns a JWT #47

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -21,4 +21,9 @@ export class AuthService {
         }
         return null;
     }
+
+    async login(user: any) {
+        const payload = { email: user.email, sub: user.userId };
+        return { access_token: this.jwtService.sign(payload), };
+    }
 }
