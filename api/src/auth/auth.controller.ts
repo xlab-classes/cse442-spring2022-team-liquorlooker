7e28b0b4ad45201c58ae653732beabe661353d9d
MIchael Focacci <michaelfocacci@gmail.com>
2022-03-10 22:42:39 -0500

    Create AuthController #51

---
+++ api/src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import {
+    ClassSerializerInterceptor,
+    Controller,
+    UseInterceptors,
+    Post,
+    UseGuards,
+    Request,
+    Body
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { LocalAuthGuard } from "./local.auth-guard";
+import { AuthDto } from "./auth.dto";
+import { User } from "src/users/users.entity";
+
+
+@Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() credentials: AuthDto): Promise<User> {
+    const { email, password } = credentials;
+    return await this.authService.register(email, password);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+}
