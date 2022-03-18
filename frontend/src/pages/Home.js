61612df1ae8b661545f7ca98b291c329f862fa35
Naqi Haider <naqihaid@buffalo.edu>
2022-03-18 00:40:46 -0400

    Added a dropdown to search #56

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,12 +5,12 @@ import MyList from "../components/List/List";
 import Map from "../images/maps.png";
 import data from "../components/List/store.json";
 import { Helmet } from "react-helmet";
+import Autocomplete from "@mui/material/Autocomplete";
+import { Paper } from "@mui/material";
 
 const Home = () => {
-
   return (
     <body className="main">
-
       <Helmet>
         <style>{"body { background-color: #363636; }"}</style>
       </Helmet>
@@ -28,7 +28,7 @@ const Home = () => {
             InputProps={{
               style: { color: "azure" },
             }}
-            defaultValue= "5"
+            defaultValue="5"
             onChange={(event) =>
               event.target.value < 1
                 ? (event.target.value = "")
@@ -42,24 +42,44 @@ const Home = () => {
 
       <div className="right">
         <div className="search">
-          <TextField
-            id="search-tf"
-            label="Search Drink"
-            variant="outlined"
-            fullWidth="true"
+          <Autocomplete
+          id="combo-box"
+            freeSolo
+            disableClearable
             size="small"
-            InputLabelProps={{
-              style: { color: "azure" },
-            }}
-            InputProps={{
-              style: { color: "azure" },
-            }}
-            type="search"
+            PaperComponent={({ children }) => (
+              <Paper
+                style={{
+                  background: "#363636",
+                  color: "azure",
+                }}
+              >
+                {children}
+              </Paper>
+            )}
+            options={data.map((option) => option.text)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                id="search-tf"
+                label="Search Drink"
+                fullWidth="true"
+                // size="small"
+                InputLabelProps={{
+                  style: { color: "azure" },
+                }}
+                InputProps={{
+                  ...params.InputProps,
+                  style: { color: "azure" },
+                  type: "search",
+                }}
+              />
+            )}
           />
         </div>
+
         <img className="map" src={Map} alt="Map" />
       </div>
-
     </body>
   );
 };
