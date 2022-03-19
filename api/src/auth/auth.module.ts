84ca9d41cb9850d0ec472f6e1928570c93443088
David Livadhi <Livadhi.david@gmail.com>
2022-03-19 19:00:06 -0400

    Removed merge conflicts #64

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -20,14 +20,9 @@ import { JwtStrategy } from './jwt/jwt.strategy';
       }),
       inject: [ConfigService],
     }),
-<<<<<<< HEAD
-],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-=======
     ConfigModule,
   ],
   providers: [AuthService, LocalStrategy],
->>>>>>> sprint2-issue#64
   controllers: [AuthController],
   exports: [AuthService, JwtModule],
 })
