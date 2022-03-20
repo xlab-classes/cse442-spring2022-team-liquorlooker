4417042ea1bde1ba0146e19692e49382840c8d51
SeanTimmons <smtimmon@buffalo.edu>
2022-03-20 18:50:22 -0400

    Added CORS to main.ts #54

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,9 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const options = {
+    "origin": '*', 
+    "methods": ['GET', 'POST', 'UPDATE', 'DEL'],
+    "allowedHeaders": ['Content-type']
+    // "preflightContinue": false,
+    // "optionsSuccessStatus": 204
+  };
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('PORT');
+  app.enableCors(options);
   await app.listen(port);
   await console.log(`Listening on port: ${port}`);
 }
