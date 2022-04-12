6c8eeb48943f63ce593ea324190134e0ca574435
SeanTimmons <smtimmon@buffalo.edu>
2022-04-12 11:22:56 -0400

    Connected Edit Drink Page #70

---
+++ frontend/src/hooks/use-drink-exists.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const UseDrinkExists = (drinkName) => {
+  const [drinkExists, setStoreData] = useState(false);
+
+  const fetchDrinkExists = async () => {
+    var axios = require('axios');
+    var qs = require('qs');
+    var data = qs.stringify({
+      'drinkName': drinkName
+    });
+    var config = {
+      method: 'get',
+      url: 'http://localhost:3000/drinks/exists',
+      headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      setStoreData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    fetchDrinkExists();
+  }, [drinkName]);
+
+  return drinkExists;
+};
+
+export default UseDrinkExists;
--- frontend/src/pages/EditDrinkInfo.js
+++ frontend/src/pages/EditDrinkInfo.js
@@ -1,10 +1,118 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import styles from "../styles/EditDrinkInfo.module.css";
 import { TextField } from "@mui/material";
+import UseDrinkExists from "../hooks/use-drink-exists"
+
+async function addEditDrink(drinkName, drinkPrice, drinkExists) {
+  console.log(drinkExists);
+  if(drinkExists == false){
+    var axios = require('axios');
+    var qs = require('qs');
+    var data = qs.stringify({
+      'drinkName': drinkName
+    });
+    var config = {
+      method: 'post',
+      url: 'http://localhost:3000/drinks/addDrink',
+      headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      newDrinkAddEdit(drinkName, drinkPrice);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  var axios = require('axios');
+  var qs = require('qs');
+  var data = qs.stringify({
+    'storeName': 'Premier',
+    'drinkName': drinkName,
+    'drinkPrice': drinkPrice 
+  });
+  var config = {
+    method: 'post',
+    url: 'http://localhost:3000/drinkPrice/addDrinkPrice',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+async function newDrinkAddEdit(drinkName, drinkPrice){
+  var axios = require('axios');
+  var qs = require('qs');
+  var data = qs.stringify({
+    'storeName': 'Premier',
+    'drinkName': drinkName,
+    'drinkPrice': drinkPrice 
+  });
+  var config = {
+    method: 'post',
+    url: 'http://localhost:3000/drinkPrice/addDrinkPrice',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+async function removeDrink(drinkName) {
+  var axios = require('axios');
+  var qs = require('qs');
+  var data = qs.stringify({
+    'storeName': 'Premier',
+    'drinkName': drinkName
+  });
+  var config = {
+    method: 'delete',
+    url: 'http://localhost:3000/drinkPrice/deleteDrinkPrice',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
 const EditDrinkInfo = () => {
+  const [drinkName, setDrinkName] = useState("");
+  const [drinkPrice, setDrinkPrice] = useState("");
+  const drinkExistsBool = UseDrinkExists(drinkName).drinkExists;
+
   return(
     <main>
       <Helmet>
@@ -25,6 +133,8 @@ const EditDrinkInfo = () => {
             InputLabelProps={{
               style: { color: "azure" },
             }}
+            value={drinkName}
+            onChange={(event) => setDrinkName(event.target.value)}
           />
         </div>
       </div>
@@ -38,25 +148,27 @@ const EditDrinkInfo = () => {
             InputLabelProps={{
               style: { color: "azure" },
             }}
+            value={drinkPrice}
+            onChange={(event) => setDrinkPrice(event.target.value)}
           />
         </div>
       </div>
 
       <div className={styles.AddDrinkButton}>
         <Link to="/businessInventory">
-          <button type="submit">Add Drink</button>
+          <button 
+            type="submit"
+            onClick={() => addEditDrink(drinkName, drinkPrice, drinkExistsBool)}
+          >Add/Edit Drink</button>
         </Link>
       </div>
 
       <div className={styles.RemoveDrinkButton}>
         <Link to="/businessInventory">
-          <button type="submit">Remove Drink</button>
-        </Link>
-      </div>
-
-      <div className={styles.EditDrinkButton}>
-        <Link to="/businessInventory">
-          <button type="submit">Edit Drink</button>
+          <button 
+            type="submit"
+            onClick={() => removeDrink(drinkName)}
+          >Remove Drink</button>
         </Link>
       </div>
     </main>
