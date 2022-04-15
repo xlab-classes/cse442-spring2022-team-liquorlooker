11f3df17289b61329cdf70c04c4c237c199f546b
David Livadhi <Livadhi.david@gmail.com>
2022-04-14 23:10:40 -0400

    Fixed bcrypt and validate user #73

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -2,28 +2,33 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/users.entity';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly usersService: UsersService,
-        private readonly jwtService: JwtService
-    ) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async register(email: string, password: string): Promise<User> {
-        return await this.usersService.createUser(email, password);
-    }
+  async register(email: string, password: string): Promise<User> {
+    return await this.usersService.createUser(email, password);
+  }
 
-    async validateUser(email: string, password: string): Promise<User> {
-        const user = await this.usersService.getUserByEmail(email);
-        if (user && user.password === password) {
-            return user;
-        }
-        return null;
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.usersService.getUserByEmail(email);
+    const hash = user.password;
+    try {
+      const isMatch = await bcrypt.compare(password, hash);
+      if (isMatch) {
+        return user;
+      }
+    } catch (error) {
+      return error;
     }
+  }
 
-    async login(user: any) {
-        const payload = { email: user.email, sub: user.userId };
-        return { access_token: this.jwtService.sign(payload), };
-    }
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    return { access_token: this.jwtService.sign(payload) };
+  }
 }
