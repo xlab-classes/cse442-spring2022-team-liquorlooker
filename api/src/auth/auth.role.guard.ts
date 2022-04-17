d2964733c2ec1ff852789faa88128cf3bd8ff3c4
Michael Focacci <michaelfocacci@gmail.com>
2022-04-17 18:58:24 -0400

    Role Guard compares user role to required roles #62

--- api/src/auth/auth.role.guard.ts
+++ api/src/auth/auth.role.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from './auth.role.enum';
+import { User } from 'src/users/users.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector,
+    private usersService: UsersService) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
@@ -15,6 +18,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.role == role);
+    const actualUser = await this.usersService.getUserByEmail(user.email);
+    return requiredRoles.includes(actualUser.role);
   }
 }
