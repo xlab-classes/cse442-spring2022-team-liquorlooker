82ec14a2c54ab12784430ccd4fd2fa71e88a1fe7
David Livadhi <Livadhi.david@gmail.com>
2022-04-17 19:20:38 -0400

    added https code to main #77

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as fs from 'fs';
 async function bootstrap() {
   const options = {
-    "origin": '*', 
-    "methods": ['GET', 'POST', 'UPDATE', 'DELETE'],
-    "allowedHeaders": ['Content-type']
+    origin: '*',
+    methods: ['GET', 'POST', 'UPDATE', 'DELETE'],
+    allowedHeaders: ['Content-type'],
     // "preflightContinue": false,
     // "optionsSuccessStatus": 204
   };
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('PORT');
   app.enableCors(options);
