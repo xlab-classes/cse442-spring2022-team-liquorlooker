076692be9b374b3e2747d70a0dec03a5b02eb57b
Naqi Haider <naqihaid@buffalo.edu>
2022-04-26 14:20:28 -0400

    Updated DrinkDetail to page to add comments #78

--- frontend/src/pages/DrinkDetail.js
+++ frontend/src/pages/DrinkDetail.js
@@ -1,23 +1,33 @@
 import React from "react";
-import styles from "../styles/DrinkDetailCSS.module.css"
-import {Helmet} from "react-helmet"
-import beer from "../images/download.jpg"
+import styles from "../styles/DrinkDetailCSS.module.css";
+import { Helmet } from "react-helmet";
+import beer from "../images/download.jpg";
+import Comments from "../components/Comments/Comments";
+import { ButtonGroup, IconButton } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 
 const DrinkDetail = () => {
   return (
     <body>
       <div>
         <Helmet>
-          <style>{'body {background-color: #363636; }'}</style>
+          <style>{"body {background-color: #363636; }"}</style>
         </Helmet>
-        <img src={beer} alt="Beer" className={styles.DrinkImage}></img>
-        <p><span className={styles.DrinkName}>Drink Name</span></p>
-        <p><span className={styles.Price}>Price: $$$</span></p>
-        <p><span className={styles.Locations}>Locations: 1,2,3</span></p>
-        <p><span className={styles.Rating}>Rating: x/10</span></p>
+
+        <h1 style={{ color: "azure" }}>GIN</h1>
+        <ButtonGroup variant="text" aria-label="text button group">
+          <IconButton sx={{ color: "azure" }}>
+            <ThumbUpIcon fontSize="large" />
+          </IconButton>
+          <IconButton sx={{ color: "azure" }}>
+            <ThumbDownIcon fontSize="large" />
+          </IconButton>
+        </ButtonGroup>
+        <Comments />
       </div>
     </body>
   );
-}
+};
 
 export default DrinkDetail;
