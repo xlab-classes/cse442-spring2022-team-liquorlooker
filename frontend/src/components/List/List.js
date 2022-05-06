de0e3ceda0807477f302cc07d74b0de8751f4330
Naqi Haider <naqihaid@buffalo.edu>
2022-05-06 16:10:45 -0400

    Fixed rendering, page forwarding bug for drink search #88

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -4,8 +4,10 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { ListItemButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import useDrinkExists from "../../hooks/use-drink-exists";
 
 const MyList = (props) => {
+  const drinkExists = useDrinkExists(props.drinkName);
   const compareByPrice = (a, b) => {
     return a.drinkPrice > b.drinkPrice
       ? 1
@@ -27,78 +29,112 @@ const MyList = (props) => {
   // )
   // }
 
-  if (stores.length > 0) {
-    return (
-      <List
-        sx={{
-          width: "100%",
-          color: "azure",
-          bgcolor: "#363636",
-          position: "relative",
-          overflow: "auto",
-          maxHeight: "80vh",
-          minWidth: "20vw",
-          maxWidth: "100vw",
-          overflow: "auto",
-          border: 1,
-          borderColor: "white",
-          borderRadius: 2,
-          "& ul": { padding: 0 },
-        }}
-      >
-        <ul>
-          {stores.map?.((store) => {
-            return (
-              <ListItem key={store}>
-                <Link
-                  to={"/DrinkDetail"}
-                  state={{ drinkName: props.drinkName }}
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  <ListItemButton>
-                    <ListItemText
-                      primary={`${store.storeName} \n ${
-                        store.drinkName
-                      } $${store.drinkPrice.toFixed(2)}`}
-                    />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-            );
-          })}
-        </ul>
-      </List>
-    );
-  } else {
-    return (
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          color: "azure",
-          bgcolor: "#363636",
-          position: "relative",
-          overflow: "auto",
-          maxHeight: "79vh",
-          minWidth: "20vw",
-          maxWidth: "100vw",
-          overflow: "auto",
-          border: 1,
-          borderColor: "white",
-          borderRadius: 2,
-          "& ul": { padding: 0 },
-        }}
-      >
-        <ul>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText primary={"No Price Detected"} />
-            </ListItemButton>
-          </ListItem>
-        </ul>
-      </List>
-    );
-  }
+  // if (stores.length > 0) {
+    console.log(drinkExists)
+  return (
+    <>
+      {drinkExists && stores.length > 0 && (
+        <List
+          sx={{
+            width: "100%",
+            color: "azure",
+            bgcolor: "#363636",
+            position: "relative",
+            overflow: "auto",
+            maxHeight: "80vh",
+            minWidth: "20vw",
+            maxWidth: "100vw",
+            overflow: "auto",
+            border: 1,
+            borderColor: "white",
+            borderRadius: 2,
+            "& ul": { padding: 0 },
+          }}
+        >
+          <ul>
+            {stores.map?.((store) => {
+              return (
+                <ListItem key={store}>
+                  <Link
+                    to={"/DrinkDetail"}
+                    state={{ drinkName: props.drinkName }}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <ListItemButton>
+                      <ListItemText
+                        primary={`${store.storeName} \n ${
+                          store.drinkName
+                        } $${store.drinkPrice.toFixed(2)}`}
+                      />
+                    </ListItemButton>
+                  </Link>
+                </ListItem>
+              );
+            })}
+          </ul>
+        </List>
+      )}
+      {!drinkExists && (
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            color: "azure",
+            bgcolor: "#363636",
+            position: "relative",
+            overflow: "auto",
+            maxHeight: "79vh",
+            minWidth: "20vw",
+            maxWidth: "100vw",
+            overflow: "auto",
+            border: 1,
+            borderColor: "white",
+            borderRadius: 2,
+            "& ul": { padding: 0 },
+          }}
+        >
+          <ul>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary={"No Price Detected"} />
+              </ListItemButton>
+            </ListItem>
+          </ul>
+        </List>
+      )}
+    </>
+  );
 };
+//   } else {
+//     return (
+//       <List
+//         sx={{
+//           width: "100%",
+//           maxWidth: 360,
+//           color: "azure",
+//           bgcolor: "#363636",
+//           position: "relative",
+//           overflow: "auto",
+//           maxHeight: "79vh",
+//           minWidth: "20vw",
+//           maxWidth: "100vw",
+//           overflow: "auto",
+//           border: 1,
+//           borderColor: "white",
+//           borderRadius: 2,
+//           "& ul": { padding: 0 },
+//         }}
+//       >
+//         <ul>
+//           <ListItem>
+//             <ListItemButton>
+//               <ListItemText primary={"No Price Detected"} />
+//             </ListItemButton>
+//           </ListItem>
+//         </ul>
+//       </List>
+//     );
+//   }
+// };
 
 export default MyList;
--- frontend/src/hooks/use-drink-exists.js
+++ frontend/src/hooks/use-drink-exists.js
@@ -1,27 +1,25 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-const UseDrinkExists = (drinkName) => {
-  const [drinkExists, setStoreData] = useState(false);
+export default function useDrinkExists(drinkName) {
+  const [drinkExists, setDrinkExists] = useState(false);
 
   const fetchDrinkExists = async () => {
-    var axios = require('axios');
-    var qs = require('qs');
-    var data = qs.stringify({
-      'drinkName': drinkName
-    });
     var config = {
       method: 'get',
       url: `http://${process.env.REACT_APP_DEV_URL}/drinks/exists`,
       headers: { 
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      data : data
+      params : {
+        drinkName : drinkName,
+      }
     };
 
     axios(config)
     .then(function (response) {
       console.log(JSON.stringify(response.data));
-      setStoreData(response.data);
+      setDrinkExists(response.data);
     })
     .catch(function (error) {
       console.log(error);
@@ -32,7 +30,5 @@ const UseDrinkExists = (drinkName) => {
     fetchDrinkExists();
   }, [drinkName]);
 
-  return drinkExists;
+  return drinkExists.drinkExists;
 };
-
-export default UseDrinkExists;
